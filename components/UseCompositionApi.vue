490be3ab003fac459f5ac28b4c84604a6096138d
key <seodaeya@gmail.com>
2022-11-11 16:22:03 +0900

    nuxt 프로젝트 composition-api 사용

---
+++ components/UseCompositionApi.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>숫자</h1>
+    <input type="text" :value="num" />
+    <h1>글자</h1>
+    <input type="text" :value="txt" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+
+  setup() {
+    const num: number = 1
+    const txt: string = 'text'
+
+    return { num, txt }
+  },
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>#CompositionApi</h1>
+    <use-composition-api />
+    <h1>#Tutorial</h1>
+    <Tutorial />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
